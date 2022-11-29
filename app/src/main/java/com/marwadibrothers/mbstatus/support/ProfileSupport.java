b51a1bb91f5d153bba2e17534fb268a249a77c57
jayaprasad356 <jayaprasad356@gmail.com>
2022-11-29 15:19:30 +0530

    minor changes updated

--- app/src/main/java/com/marwadibrothers/mbstatus/support/ProfileSupport.java
+++ app/src/main/java/com/marwadibrothers/mbstatus/support/ProfileSupport.java
@@ -402,6 +402,7 @@ public class ProfileSupport {
         CropImage.activity()
                 .setGuidelines(CropImageView.Guidelines.ON)
                 .setOutputCompressFormat(Bitmap.CompressFormat.PNG)
+                .setOutputCompressQuality(50)
                 .setAspectRatio(1,1)
                 .start(activity);
 
